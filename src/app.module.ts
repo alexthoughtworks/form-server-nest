9b84e9d2f05690f58666bdf80c849786cb2f2595
Juan Sulca <lajuan.sulca@MacBook-Pro-de-Juan.local>
2022-05-25 16:05:06 -0500

    installed typeorm and gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', //'squad-instance.cazry4tkvt97.us-east-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'postgres',
+      password: 'mysecretpassword',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
